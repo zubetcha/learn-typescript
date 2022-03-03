15930b99b220535f2462fbc0da5d945881864c1f
zubetcha <zuhye5@gmail.com>
2022-03-03 21:23:36 +0900

    first commit

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,25 +1,26 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = 'https://jsonplaceholder.typicode.com/users/1'
 
 // dom
-var username = document.querySelector('#username');
-var email = document.querySelector('#email');
-var address = document.querySelector('#address');
+var username = document.querySelector('#username')
+var email = document.querySelector('#email')
+var address = document.querySelector('#address')
 
 // user data
-var user = {};
+var user = {}
 
 function startApp() {
   axios
     .get(url)
     .then(function (response) {
-      console.log(response);
-      user = response.data;
+      // console.log(response)
+      user = response.data
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user)
     })
     .catch(function (error) {
-      console.log(error);
-    });
+      console.log(error)
+    })
 }
 
-startApp();
+startApp()
