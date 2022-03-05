c7e87c1d058268ae390a7449534332d204eedf8c
zubetcha <zuhye5@gmail.com>
2022-03-05 15:59:41 +0900

    타입스크립트 기본 타입 - 문자열, 숫자, 배열

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,14 @@
+// JS 문지열 선언
+// var str = 'hello'
+
+// TS 문자열 선언 > 변수의 오른쪽에 콜론과 함께 타입 지정
+let str: string = 'hello'
+
+// TS 숫자
+let num: number = 10
+
+// TS 배열
+let arr: Array<number> = [1, 2, 3] // arr는 Array type이며, 요소로서 number만 가질 수 있음
+let items: number[] = [3, 4, 5] // 배열을 리터럴로 표현한 형태로, : Array<number> 와 동일한 타입
+
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk']
