32746d1be5fd99d969c46dae0857d21b1ce8961c
zubetcha <zuhye5@gmail.com>
2022-03-05 17:48:00 +0900

    타입스크립트 함수 타입 - 옵셔널 파라미터

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -14,3 +14,12 @@ function add2(): number {
 function sum2(a: number, b: number): number {
   return a + b
 }
+
+// TS 함수의 파라미터를 제한하는 특성
+// sum2(10, 20, 30, 40) // JS는 함수에 정의되어 있지 않은 세 번째, 네 번째 인자를 넘겨도 유연하게 대처함
+// sum2(10) // 들어와야 하는 인자의 개수가 일치하는지 엄격하게 체크함
+
+// TS 함수의 옵셔널 파라미터 > 물음표를 붙이면 필요에 따라 함수의 파라미터 개수를 유연하게 설정할 수 있음
+function log(a: string, b?: string) {}
+log('hello world')
+log('hello ts', 'abc')
