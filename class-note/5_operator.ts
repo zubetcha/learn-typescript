dac1fbeb4bfcaa113a9eeca11d6d5e45b041b8d5
zubetcha <zuhye5@gmail.com>
2022-03-07 21:32:07 +0900

    타입스크립트 인터섹션 타입

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -20,3 +20,34 @@ function logMessage(value: string | number) {
 }
 logMessage('hello')
 logMessage(100)
+
+interface Developer {
+  name: string;
+  skill: string;
+}
+
+interface Person2 {
+  name: string;
+  age: number;
+}
+
+// 인터페이스 두 개를 유니온 타입으로 연결할 때에는 공통된 속성에만 접근할 수 있음
+function askSomeone(someone: Developer | Person2) {
+  // someone.name
+  // someone.skill // error, skill 속성의 타입을 보장하지 않음
+  // someone.age // error, age 속성의 타입을 보장하지 않음
+}
+
+askSomeone({name: '디벨로퍼', skill: '웹 개발'})
+askSomeone({name: '주주', age: 29})
+
+// 인터섹션 타입
+
+function askSomeone2(someone: Developer & Person2) { // someone은 Developer와 Person2이 가지고 있는 모든 속성과 타입을 포함하는 하나의 타입 > 인터섹션
+  someone.name
+  someone.skill 
+  someone.age  
+}
+
+askSomeone2({name: '디벨로퍼', skill: '웹 개발', age: 29})
+// askSomeone2({name: '주주', age: 29})
