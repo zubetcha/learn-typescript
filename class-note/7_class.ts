6ca3eed3cab14f9ba1f047b438ed8b146b7dfc21
zuhye <zuhye@zuhyeui-MacBookPro.local>
2022-03-19 08:20:34 +0900

    syntax of class in TS

--- class-note/7_class.ts
+++ class-note/7_class.ts
@@ -3,7 +3,7 @@
 
 // 클래스의 프로퍼티는 타입뿐만 아니라 유효범위까지 지정할 수 있음
 // default는 public이며, 클래스 안에서만 사용할 거라면 private 키워드를 앞에 붙이면 됨
-// 그 외에도 protected, readonly 등이 있음
+// 그 외에도 protected, readonly 등이 있다.
 
 class Person {
     private name: string;
