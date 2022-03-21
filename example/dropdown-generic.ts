a7ac0486e288ec32ad96b4c80b76503b22cae810
zubetcha <zuhye5@gmail.com>
2022-03-21 22:44:49 +0900

    type definition using generic

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,16 +1,31 @@
-const emails = [
+// 제네릭을 이용하면 하나의 인터페이스로 여러가지 코드에 유연하게 대응할 수 있다.
+interface DropdownItem<T> {
+  value: T;
+  selected: boolean;
+}
+// interface Email {
+//   value: string;
+//   selected: boolean;
+// }
+
+const emails: DropdownItem<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts = [
+// interface ProductNumber {
+//   value: number;
+//   selected: boolean;
+// }
+
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
+function createDropdownItem(item: DropdownItem<string> | DropdownItem<number>) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -23,4 +38,8 @@ emails.forEach(function (email) {
   const item = createDropdownItem(email);
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
+});
+
+numberOfProducts.forEach(function (product) {
+  const item = createDropdownItem(product)
 });
